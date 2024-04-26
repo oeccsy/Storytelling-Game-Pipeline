b012d148852096eb76029071a6e80833f788e8f5
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-04-26 18:30:19 +0900

    feat : subtitle

---
+++ Project/Unity/Game/Assets/Scripts/UI/Subtitle.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class Subtitle : MonoBehaviour
+    {
+        private TextMeshProUGUI _textUI;
+
+        private void Awake()
+        {
+            _textUI = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        public void SetText(string str)
+        {
+            _textUI.text = str;
+        }
+
+        public void RemoveText()
+        {
+            _textUI.text = "";
+        }
+    }
+}
